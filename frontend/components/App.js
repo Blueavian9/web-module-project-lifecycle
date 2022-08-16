14d0fa9b49d15feb05d235c629423020bf3ca622
cesar.aguilar <Blueavian9@gmail.com>
2022-08-15 22:29:15 -0700

    final commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,87 @@
+import TodoList from './TodoList'
+import Form from './Form'
 import React from 'react'
+import axios from 'axios'
+
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: '',
+    displayCompleteds: true,
+  }
+
+  nameChangeInput = event => {
+    const { value } = event.target;
+    this.setState({ ...this.state, todoNameInput: value })
+  }
+
+  resetForm = () => this.setState({ ...this.state, todoNameInput: '' });
+
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message });
+
+  postNewTodo = () => {
+    axios.post(URL, { name: this.state.todoNameInput })
+      .then(res => {
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+        this.resetForm();
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  onTodoFormSubmit = event => {
+    event.preventDefault();
+    this.postNewTodo();
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td
+            return res.data.data;
+          })
+        })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleDisplayCompleteds = () => {
+    this.setState({ ...this.state, displayCompleteds: !this.state.displayCompleteds })
+  }
+
+  componentDidMount() {
+    this.fetchAllTodos();
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: {this.state.error}</div>
+        <TodoList
+          todos={this.state.todos}
+          displayCompleteds={this.state.displayCompleteds}
+          toggleCompleted={this.toggleCompleted}
+        />
+        <Form
+          onTodoFormSubmit={this.onTodoFormSubmit}
+          nameChangeInput={this.nameChangeInput}
+          toggleDisplayCompleteds={this.toggleDisplayCompleteds}
+          todoNameInput={this.state.todoNameInput}
+          displayCompleteds={this.state.displayCompleteds}
+        />
+      </div>
+    )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    return (
+      <div
+        onClick={this.props.toggleCompleted(this.props.td.id)}
+      >
+        {this.props.td.name}
+        {this.props.td.completed ? ' ✔️' : ''}
+      </div>
+    )
   }
 }
